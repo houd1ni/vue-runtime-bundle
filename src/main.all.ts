b7e5d57d8b0f1e2e5b78fb2bb51ce999e837a25b
Michael <akiliev@outlook.com>
2019-03-25 18:42:25 +0300

    versions update, fela-vue lit-css.

--- src/main.all.ts
+++ src/main.all.ts
@@ -1,12 +1,13 @@
 
 import Vue from 'vue/dist/vue.esm.browser'
 import VueRouter from 'vue-router'
-import { Renderer } from 'fela-vue'
+import { Renderer, css as felaCss } from 'fela-vue'
 import { html, css, cssinject } from './literals'
 
 ;(window as any).RUNTIME = {
   Vue,
   VueRouter,
   fela: new Renderer(),
+  felaCss,
   literals: { html, css, cssinject }
 }
--- src/main.fela.ts
+++ src/main.fela.ts
@@ -1,12 +1,12 @@
 
 import Vue from 'vue/dist/vue.esm.browser'
 import VueRouter from 'vue-router'
-import { Renderer } from 'fela-vue'
+import { Renderer, css } from 'fela-vue'
 import { html } from './literals'
 
 ;(window as any).RUNTIME = {
   Vue,
   VueRouter,
   fela: new Renderer(),
-  literals: { html }
+  literals: { html, css }
 }
